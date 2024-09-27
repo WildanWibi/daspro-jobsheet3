c7b88d29c43f4499b43840624a3c62318ae776be
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-09-27 10:17:03 +0700

    JobSheet 3

--- ModifikasiSiAkad.java
+++ ModifikasiSiAkad.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class SiAkad14 {
+public class ModifikasiSiAkad {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
         String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
-        
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
+        double bobotKuis = 0.20, bobotTugas = 0.15, bobotUTS = 0.30, bobotUAS = 0.35;
+
         System.out.print("Masukkan Nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan Nim: ");
@@ -18,18 +19,19 @@ public class SiAkad14 {
         System.out.print("Masukkan Absen: ");
         absen = sc.nextByte();
 
-        System.out.print("Masukkan nilai Kuis: ");
+        System.out.print("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
-        System.out.print("Masukkan nilai Tugas: ");
+        System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai Ujian: ");
-        nilaiUjian = sc.nextDouble();
-
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis * bobotKuis) + (nilaiTugas * bobotTugas) + (nilaiUTS * bobotUTS) + (nilaiUAS * bobotUAS);
 
         System.out.println("Mahasiswa dengan nama " + nama + " (NIM " + nim +")");
         System.out.println("Kelas " + kelas + " nomor absen " + absen);
-        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        System.out.println("Nilai akhir adalah: " + nilaiAkhir);
 
         sc.close();
     }
